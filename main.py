da2c7be7c25daae824111cb0ed9f8f673fbcb8ec
shonenada <shonenada@gmail.com>
2014-02-26 14:49:28 +0800

    walk right

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ import utils
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+N_MIN = 5
+N_MAX = 7
+STAY_STEP = 0
+WALK_STEP = 10
+STAY_SLEEP = 0
+WALK_SLEEP = 1 / 6
 
 
 def main():
@@ -28,32 +34,30 @@ def main():
     #     dennis_bmp.mode
     # )
 
-    N_MIN = 5
-    N_MAX = 7
-    n = 0
-    n_step = 0
-    time_sleep = 0
+    x_seed = 0
+    x_seed_step = 0
+    time_sleep = STAY_SLEEP
 
     s_x, s_y = 0, 0
-    s_step = 10
+    s_step_x = WALK_STEP
+    s_step_y = WALK_STEP
 
     color_fill = BLACK
+    crop_x, crop_y = 0, 0
     
-    def update_dennis(x, y):
-        cropped = utils.get_crop(dennis_bmp, (79, 79), (x, y))
+    def update_dennis():
+        cropped = utils.get_crop(dennis_bmp, (79, 79), (crop_x, crop_y))
         dennis = pygame.image.frombuffer(
             cropped.tostring(),
             cropped.size,
             cropped.mode
         )
         screen.fill(color_fill)
-        screen.blit(dennis, (0, 0))
+        screen.blit(dennis, (s_x, s_y))
         pygame.display.update()
 
-
-    crop_x, crop_y = 0, 0
-    update_dennis(crop_x, crop_y)
-
+    s_step_y = 0
+    s_step_x = 0
 
     while True:
         event = pygame.event.poll()
@@ -63,23 +67,28 @@ def main():
 
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
-                time_sleep = 1/6
-                n = N_MIN
-                n_step = 1
+                time_sleep = WALK_SLEEP
+                x_seed = N_MIN
+                x_seed_step = 1
+                s_step_x = WALK_STEP
+
         elif event.type == KEYUP:
-            n = 0
-            n_step = 0
+            x_seed = 0
+            x_seed_step = 0
+            s_step_x = 0
 
-        if n_step > 0 and n >= N_MAX:
-            n_step = -1
-        elif n_step < 0 and n <= N_MIN:
-            n_step = 1
+        if x_seed_step > 0 and x_seed >= N_MAX:
+            x_seed_step = -1
+        elif x_seed_step < 0 and x_seed <= N_MIN:
+            x_seed_step = 1
 
-        n = n + n_step
-        crop_x = n * 80
+        x_seed = x_seed + x_seed_step
+        crop_x = x_seed * 80
+        s_x = s_x + s_step_x
+        s_y = s_y + s_step_y
 
-        time.sleep( time_sleep )
-        update_dennis(crop_x, crop_y)
+        time.sleep ( time_sleep )
+        update_dennis()
 
 
 if __name__ == '__main__':
